23d3e8fdd7993f24adeb6007e605e9462a66b8b1
Danyc <44319695+Danyc392@users.noreply.github.com>
2025-03-17 04:14:25 +0100

    Add files via upload

---
+++ esx_menu_default/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'danizgz'
+
+description 'ESX Menu Default edited by danizgz'
+
+client_scripts {
+	'@es_extended/imports.lua',
+	'client/main.lua'
+}
+
+ui_page {
+	'html/ui.html'
+}
+
+files {
+	files {"**/**/**/**/**/**/**/*.*"}
+}
+
+dependencies {
+	'es_extended'
+}
